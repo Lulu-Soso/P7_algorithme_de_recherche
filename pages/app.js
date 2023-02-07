fa377bd2e0ce51674f1f9bcff0103b83336b718b
Lulu-Soso <lucas.sokha@gmail.com>
2023-02-07 10:58:22 +0100

    display recipes with their ingredients, appliances and utensils in dropdowns

--- pages/app.js
+++ pages/app.js
@@ -9,6 +9,9 @@ const { getRecipes, getUtensils, getAppliances} = dataApi();
 
 async function displayRecipes(recipes) {
   const cardsContainer = document.querySelector(".cards-container");
+  const ulOptionAppliances = document.querySelector(".option-appliances");
+  const ulOptionIngredients = document.querySelector(".option-ingredients");
+  const ulOptionUtensils = document.querySelector(".option-utensils");
 
   recipes.forEach((recipe) => {
     const recipeModel = recipeFactory(recipe);
@@ -16,198 +19,126 @@ async function displayRecipes(recipes) {
     recipeCardDOM.innerHTML = recipeModel.getRecipeCardDOM();
     cardsContainer.appendChild(recipeCardDOM);
   });
+
+  const uniqueAppliances = [...new Set(recipes.map(recipe => recipe.appliance))];
+  ulOptionAppliances.innerHTML = uniqueAppliances.map(appliance => `<li>${appliance}</li>`).join("");
+
+// Concatenate all the ingredients arrays from each recipe into a single array
+  const uniqueIngredients = recipes.reduce((acc, recipe) => {
+    return acc.concat(recipe.ingredients.map(ingredient => ingredient.ingredient));
+  }, []);
+
+// Use a set to get only unique values from the uniqueIngredients array and convert it back to an array
+// Generate an HTML string with a list of <li> elements, each displaying a unique ingredient
+  ulOptionIngredients.innerHTML = [...new Set(uniqueIngredients)].map(ingredient => `<li>${ingredient}</li>`).join("");
+
+// Concatenate all the utensils arrays from each recipe into a single array
+  const uniqueUtensils = recipes.reduce((acc, recipe) => {
+// Convert each utensil to uppercase and concatenate them
+    return acc.concat(recipe.ustensils.map(utensil => utensil[0].toUpperCase() + utensil.slice(1)));
+  }, []);
+
+// Use a set to get only unique values from the uniqueUtensils array and convert it back to an array
+// Generate an HTML string with a list of <li> elements, each displaying a unique utensil
+  ulOptionUtensils.innerHTML = [...new Set(uniqueUtensils)].map(utensil => `<li>${utensil}</li>`).join("");
 }
 
-// async function displayIngredients(ingredients) {
+// function displayIngredients(ingredients) {
 //   const ulOptionIngredients = document.querySelector(".option-ingredients");
 //
 //   ingredients.forEach((ingredient) => {
-//     const ingredientModel = dropdownFactory(ingredient);
-//     const ingredientDOM = document.createElement("li");
-//     ingredientDOM.innerHTML = ingredientModel.getIngredientsDOM();
-//     ulOptionIngredients.appendChild(ingredientDOM);
+//     const recipeModel = recipeFactory(ingredient);
+//     const ingredientListItem = document.createElement("li");
+//     ingredientListItem.innerHTML = recipeModel.getIngredientsDOM();
+//     ulOptionIngredients.appendChild(ingredientListItem);
 //   });
 // }
 
-// async function displayAppliances(appliances) {
+// function displayAppliances(appliance) {
 //   const ulOptionAppliances = document.querySelector(".option-appliances");
-//   // Create a set to store unique appliances
-//   const applianceSet = new Set();
 //
-//   // Loop through each appliance
-//   appliances.forEach((appli) => {
-//     // Check if the appliance already exists in the set
-//     if (!applianceSet.has(appli.appliance)) {
-//       // If not, add the appliance to the set
-//       applianceSet.add(appli.appliance);
-//       const applianceModel = dropdownFactory(appli);
-//       const applianceDOM = document.createElement("li");
-//       applianceDOM.innerHTML = applianceModel.getAppliancesDOM();
-//       ulOptionAppliances.appendChild(applianceDOM);
-//     }
-//   })
+//   appliance.forEach((appli) => {
+//     const recipeModel = recipeFactory(appli);
+//     const applianceListItem = document.createElement("li");
+//     applianceListItem.innerHTML = recipeModel.getAppliancesDOM();
+//     ulOptionAppliances.appendChild(applianceListItem);
+//   });
 // }
 
-
-// async function displayUtensils(utensils) {
+// function displayUtensils(utensils) {
 //   const ulOptionUtensils = document.querySelector(".option-utensils");
 //
 //   utensils.forEach((utensil) => {
-//     const utensilModel = dropdownFactory(utensil);
-//     const utensilDOM = document.createElement("li");
-//     utensilDOM.innerHTML = utensilModel.getUtensilsDOM();
-//     ulOptionUtensils.appendChild(utensilDOM);
+//     const recipeModel = recipeFactory(utensil);
+//     const utensilListItem = document.createElement("li");
+//     utensilListItem.innerHTML = recipeModel.getUtensilsDOM();
+//     ulOptionUtensils.appendChild(utensilListItem);
 //   });
 // }
 
+
+
+
 ////////// Display all ingredients //////////
-async function displayIngredientsList(ingredients) {
-  const ulOptionIngredients = document.querySelector(".option-ingredients")
-
-  // Create a Set to store unique ingredient names
-  const ingredientSet = new Set();
-
-  // Set the innerHTML of the selected element to the result of mapping over the ingredients array
-  ulOptionIngredients.innerHTML = ingredients
-  .map(
-    (ingred) =>
-      `
-      ${ingred.ingredients
-        // For each ingredient object, filter out ingredients that are already in the ingredientSet
-        // Pour chaque objet d'ingrédient, filtrer les ingrédients qui sont déjà dans l'IngredientSet
-        .filter(ingredLi => !ingredientSet.has(ingredLi.ingredient))
-        .map(
-          (ingredLi) => {
-             // If it has not, add it to the ingredientSet and return a list item with its name
-            ingredientSet.add(ingredLi.ingredient);
-            return `<li>${ingredLi.ingredient}</li>`
-          }
-        )
-        .join("")}
-    `
-  )
-  .join("");
-}
+// async function displayIngredientsList(ingredients) {
+//   const ulOptionIngredients = document.querySelector(".option-ingredients")
+//
+//   // Create a Set to store unique ingredient names
+//   const ingredientSet = new Set();
+//
+//   // Set the innerHTML of the selected element to the result of mapping over the ingredients array
+//   ulOptionIngredients.innerHTML = ingredients
+//   .map(
+//     (ingred) =>
+//       `
+//       ${ingred.ingredients
+//         // For each ingredient object, filter out ingredients that are already in the ingredientSet
+//         // Pour chaque objet d'ingrédient, filtrer les ingrédients qui sont déjà dans l'IngredientSet
+//         .filter(ingredLi => !ingredientSet.has(ingredLi.ingredient))
+//         .map(
+//           (ingredLi) => {
+//              // If it has not, add it to the ingredientSet and return a list item with its name
+//             ingredientSet.add(ingredLi.ingredient);
+//             return `<li>${ingredLi.ingredient}</li>`
+//           }
+//         )
+//         .join("")}
+//     `
+//   )
+//   .join("");
+// }
 
 ////////// Display all appliances //////////
-async function displayAppliancesList(appliances) {
-  const ulOptionAppliances = document.querySelector(".option-appliances");
-
-  const applianceSet = new Set();
-
-  ulOptionAppliances.innerHTML = appliances
-    .map(liAppliance => {
-      if (!applianceSet.has(liAppliance.appliance)) {
-        applianceSet.add(liAppliance.appliance);
-        return `<li>${liAppliance.appliance}</li>`;
-      }
-    })
-    .filter(Boolean)
-    .join("");
-}
-
 // async function displayAppliancesList(appliances) {
-//   let ulOptionAppliances = document.querySelector(".option-appliances");
-//   const inputAppliancesSearch = document.querySelector("#appliances");
+//   const ulOptionAppliances = document.querySelector(".option-appliances");
 //
 //   const applianceSet = new Set();
 //
-//   // ulOptionAppliances.innerHTML = appliances
-//   //     .map(liAppliance => {
-//   //       if (!applianceSet.has(liAppliance.appliance)) {
-//   //         applianceSet.add(liAppliance.appliance);
-//   //         return `<li>${liAppliance.appliance}</li>`;
-//   //       }
-//   //     })
-//   //     .filter(Boolean)
-//   //     .join("");
-//
-//
-//   inputAppliancesSearch.addEventListener("input", (e) => {
-//     const searchValue = e.target.value.toLowerCase();
-//
-//     const filteredAppliances = appliances.filter(recipe => recipe.appliance.toLowerCase().includes(searchValue));
-//     console.log(filteredAppliances)
-//     // ulOptionAppliances = filteredAppliances
-//     filteredAppliances.forEach((appli) => {
-//     const applianceModel = dropdownFactory(appli);
-//     const applianceDOM = document.createElement("li");
-//     applianceDOM.innerHTML = applianceModel.getAppliancesDOM();
-//     ulOptionAppliances.appendChild(applianceDOM);
-//     })
-//
-//   })
-//
-// }
-
-////////// Display all utensils //////////
-// function displayUtensilsList(utensils) {
-//   const ulOptionUtensils = document.querySelector(".option-utensils");
-//   const utensilSet = new Set();
-//   const uniqueUtensils = utensils
-//   console.log(uniqueUtensils)
-//     .filter(utensil => {
-//       if (!utensilSet.has(utensil.ustensils)) {
-//         utensilSet.add(utensil.ustensils);
-//         return true;
+//   ulOptionAppliances.innerHTML = appliances
+//     .map(liAppliance => {
+//       if (!applianceSet.has(liAppliance.appliance)) {
+//         applianceSet.add(liAppliance.appliance);
+//         return `<li>${liAppliance.appliance}</li>`;
 //       }
-//       return false;
 //     })
-//     .map(utensil => utensil.ustensils);
-//
-//   ulOptionUtensils.innerHTML = uniqueUtensils
-//     .map(utensil => `<li>${utensil}</li>`)
+//     .filter(Boolean)
 //     .join("");
 // }
 
 //////// Display all utensils //////////
-function displayUtensilsList(recipes) {
-  const ulOptionUtensils = document.querySelector(".option-utensils");
-
-  // Concatenate all the utensils arrays from each recipe into a single array
-  const uniqueUtensils = recipes.reduce((acc, recipe) => {
-    // Convert each utensil to uppercase and concatenate them
-    return acc.concat(recipe.ustensils.map(utensil => utensil[0].toUpperCase() + utensil.slice(1)));
-  }, []);
-
-  // Use a set to get only unique values from the uniqueUtensils array and convert it back to an array
-  // Generate an HTML string with a list of <li> elements, each displaying a unique utensil
-  ulOptionUtensils.innerHTML = [...new Set(uniqueUtensils)].map(utensil => `<li>${utensil}</li>`).join("");
-}
-
-
-
-// function displayUtensilsList(utensils) {
+// function displayUtensilsList(recipes) {
 //   const ulOptionUtensils = document.querySelector(".option-utensils");
-//   const utensilSet = new Set();
-//   const uniqueUtensils = utensils
-//       .filter(utensil => {
-//         if (!utensilSet.has(utensil.ustensils)) {
-//           utensilSet.add(utensil.ustensils);
-//           return true;
-//         }
-//         return false;
-//       })
-//       .map(utensil => utensil.ustensils);
 //
-//   ulOptionUtensils.innerHTML = uniqueUtensils
-//       .map(utensil => {
-//         if (typeof utensil === "string") {
-//           return `<li>${utensil.charAt(0).toUpperCase() + utensil.slice(1)}</li>`;
-//         }
-//         console.error(`${utensil} is not a string.`);
-//       })
-//       .join("");
+//   // Concatenate all the utensils arrays from each recipe into a single array
+//   const uniqueUtensils = recipes.reduce((acc, recipe) => {
+//     // Convert each utensil to uppercase and concatenate them
+//     return acc.concat(recipe.ustensils.map(utensil => utensil[0].toUpperCase() + utensil.slice(1)));
+//   }, []);
+//
+//   // Use a set to get only unique values from the uniqueUtensils array and convert it back to an array
+//   // Generate an HTML string with a list of <li> elements, each displaying a unique utensil
+//   ulOptionUtensils.innerHTML = [...new Set(uniqueUtensils)].map(utensil => `<li>${utensil}</li>`).join("");
 // }
-
-
-
-
-
-
-
-
-
 
 ////////// Display selected items dropdown  //////////
 async function displayItemsDropdowns() {
@@ -276,42 +207,42 @@ async function displayRecipesBySearch() {
 }
 
 
-async function displayAppliancesBySearch() {
-  const ulOptionAppliances = document.querySelector(".option-appliances");
-  const inputAppliancesSearch = document.querySelector("#appliances");
-
-  const data = await getRecipes();
-  // Utilisez `Set` pour éliminer les doublons
-  const appliances = [...new Set(data.map(appliData => appliData.appliance))];
-
-  // Ajoutez un écouteur d'événement pour l'input de recherche
-  inputAppliancesSearch.addEventListener("input", async (e) => {
-    e.preventDefault();
-    // Récupérez la valeur saisie dans l'input
-    const searchValue = e.target.value.toLowerCase();
-
-    // Filtrez les appareils qui correspondent à la valeur de recherche
-    const filteredAppliances = appliances.filter(appliance => appliance.toLowerCase().includes(searchValue));
-
-    // Si aucun appareil n'a été trouvé, affichez un message
-    if (filteredAppliances.length === 0) {
-      ulOptionAppliances.innerHTML = "<h2>Aucun résultat</h2>";
-    } else {
-      let cardsHTML = "";
-      // Bouclez sur les appareils filtrés et créez une liste HTML
-      for (const appliance of filteredAppliances) {
-        cardsHTML += `<li>${appliance}</li>`;
-      }
-      ulOptionAppliances.innerHTML = cardsHTML;
-    }
-  });
-}
+// async function displayAppliancesBySearch() {
+//   const ulOptionAppliances = document.querySelector(".option-appliances");
+//   const inputAppliancesSearch = document.querySelector("#appliances");
+//
+//   const data = await getRecipes();
+//   // Utilisez `Set` pour éliminer les doublons
+//   const appliances = [...new Set(data.map(appliData => appliData.appliance))];
+//
+//   // Ajoutez un écouteur d'événement pour l'input de recherche
+//   inputAppliancesSearch.addEventListener("input", async (e) => {
+//     e.preventDefault();
+//     // Récupérez la valeur saisie dans l'input
+//     const searchValue = e.target.value.toLowerCase();
+//
+//     // Filtrez les appareils qui correspondent à la valeur de recherche
+//     const filteredAppliances = appliances.filter(appliance => appliance.toLowerCase().includes(searchValue));
+//
+//     // Si aucun appareil n'a été trouvé, affichez un message
+//     if (filteredAppliances.length === 0) {
+//       ulOptionAppliances.innerHTML = "<h2>Aucun résultat</h2>";
+//     } else {
+//       let cardsHTML = "";
+//       // Bouclez sur les appareils filtrés et créez une liste HTML
+//       for (const appliance of filteredAppliances) {
+//         cardsHTML += `<li>${appliance}</li>`;
+//       }
+//       ulOptionAppliances.innerHTML = cardsHTML;
+//     }
+//   });
+// }
 
 async function init() {
   changeInputColorForm();
   await closeDropdowns();
   await displayRecipesBySearch();
-  await displayAppliancesBySearch()
+  // await displayAppliancesBySearch()
   await displayItemsDropdowns()
 
   getRecipes().then(displayRecipes);
@@ -319,9 +250,9 @@ async function init() {
   // getRecipes().then(displayAppliances);
   // getRecipes().then(displayUtensils);
 
-  getRecipes().then(displayIngredientsList);
-  getRecipes().then(displayAppliancesList);
-  getRecipes().then(displayUtensilsList);
+  // getRecipes().then(displayIngredientsList);
+  // getRecipes().then(displayAppliancesList);
+  // getRecipes().then(displayUtensilsList);
 
   // const { getRecipes } = dataApi();
   // const recipes = await getRecipes();
--- scripts/factories/recipe-factory.js
+++ scripts/factories/recipe-factory.js
@@ -1,12 +1,12 @@
 export function recipeFactory(data) {
-  const { name, time, ingredients, description } = data;
+  const { name, time, ingredients, description, appliance, ustensils } = data;
 
   function getRecipeCardDOM() {
     const picture = `/assets/images/recipes-name/${name.replace(/ /g,"-")}.jpg`;
 
     return `
         <div class="card">
-          <div class="div-img"><img src="${picture}" alt="${name}" aria-label="Nom de la recette ${name}"></div>
+          <div class="div-img"><img src="${ picture}" alt="${name}" aria-label="Nom de la recette ${name}"></div>
           <div class="card-title">
             <h2>${name}</h2>
             <div class="time">
@@ -18,11 +18,11 @@ export function recipeFactory(data) {
             <div class="left-content">
               <div class="text-content-left">
               ${ingredients
-                .map(
-                  (ingredient) =>
-                    `<p>${ingredient.ingredient}: ${ ingredient.quantity ? ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""}</p>`
-                )
-                .join("")}
+        .map(
+            (ingredient) =>
+                `<p>${ingredient.ingredient}: ${ ingredient.quantity ? ingredient.quantity : ""} ${ingredient.unit ? ingredient.unit : ""}</p>`
+        )
+        .join("")}
               </div>
             </div>
             <div class="right-content">
@@ -35,11 +35,27 @@ export function recipeFactory(data) {
       `;
   }
 
+  function getIngredientsDOM() {
+    return ingredients.map(ingredient => `${ingredient.ingredient}`).join("");
+  }
+
+  function getAppliancesDOM() {
+    return `${appliance}`;
+  }
+
+  function getUtensilsDOM() {
+    return ustensils.map(utensil => `${utensil}`).join("");
+  }
+
   return {
     name,
     time,
     ingredients,
     description,
     getRecipeCardDOM,
+    getIngredientsDOM,
+    getAppliancesDOM,
+    getUtensilsDOM,
   };
 }
+
